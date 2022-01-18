163e3e334003a31fb1ca31973f0cbcc222a436a9
unknown <907062613@qq.com>
2022-01-18 21:59:44 +0800

    dierci commit

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="app-wrapper">
+		<div class="header-wrapper">
+			<Navbar/>
+		</div>		
+		<div class="main-wrapper"></div>
+	</div>	
+</template>
+<script type="text/javascript">
+	import {Navbar} from 'views/layout';
+	export default{
+		name:'layout',
+	}
+</script>
+<style type="text/css">
+	
+</style>
---
+++ src/views/layout/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+	<el-header>
+		<div class="head-wrapper">
+			<div class="head-container">
+				<div class="logo">
+									
+				</div>				
+				<div class="nav">
+					
+				</div>
+				<div class="userinfo">
+					
+				</div>
+			</div>						
+		</div>			
+	</el-header>	
+</template>
+<script type="text/javascript">
+	export default{
+
+		data(){
+			return{
+
+			}
+		},
+		methods:{
+
+		},
+
+	}	
+</script>
+<style type="text/css">
+	
+</style>
